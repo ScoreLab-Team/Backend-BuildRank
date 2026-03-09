016117d550ba840362d2af39a9f9313301c57864
e-d-u1 <eduard.corrons@estudiantat.upc.edu>
2026-03-09 20:21:26 +0100

    Add: Prova calcul BHS

---
+++ src/scoring/versions.py
@@ -0,0 +1,13 @@
+# scoring/versions.py
+
+BHS_VERSIONS = {
+    "1.0": {
+        "pes_consum": 0.4,
+        "pes_emissions": 0.3,
+        "pes_aillament": 0.2,
+        "pes_rehabilitacio": 0.1,
+        "comentari": "MVP inicial: sol consumo, emisions, aillament y rehabilitació"
+    },
+    # futures versions:
+    # "2.0": {...}
+}

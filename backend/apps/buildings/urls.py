a1c2ffa85b36651e0394df7bf7ef9104d6737b65
eduard.corrons <eduard.corrons@estudiantat.upc.edu>
2026-03-25 18:07:34 +0100

    Feature/apps buildings (#27)
    * Relocate ports
    
    * Change: Grup comparable opcional
    
    * Fix: Edifici migration
    
    * Fix: Buildings uml coherence
    
    * Relocate ports
    
    * Fix: Accounts tests
    
    * merge migrations
    
    * Merging
    
    * Updated: Tests according to DB
    
    * Relocate ports to github
    
    * Relocate CI ports
    
    * Add: API Geolocalization verification
    
    * Add: DB with carrers and %ILIKE% SQL Searach
    
    * No usar API externa
    
    * Migrations suquash
    
    * Relocate ports
    
    * Permission to view building details
    
    * List+Detail endpoints for different level of app verification
    
    * Different permissions in CRUD
    
    * Singals al canviar dadesEnergetiques
    
    * Permissions segueix ara RBAC i ABAC de /apps/accounts
    
    * Relocate Ports

--- backend/apps/buildings/urls.py
+++ backend/apps/buildings/urls.py
@@ -1,10 +1,10 @@
 # apps/buildings/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EdificiDetailAPIView, EdificiListAPIView, HabitatgeViewSet, LocalitzacioViewSet, DadesEnergetiquesViewSet, autocomplete_carrers
+from .views import EdificiDetailAPIView, EdificiViewSet, EdificiListAPIView, HabitatgeViewSet, LocalitzacioViewSet, DadesEnergetiquesViewSet, autocomplete_carrers
 
 router = DefaultRouter()
-# router.register(r'edificis', EdificiViewSet, basename='edifici')
+router.register(r'edificis', EdificiViewSet, basename='edifici')
 router.register(r'habitatges', HabitatgeViewSet, basename='habitatge')
 router.register(r'localitzacions', LocalitzacioViewSet, basename='localitzacio')
 router.register(r'dades_energetiques', DadesEnergetiquesViewSet, basename='dades-energetiques')
@@ -15,6 +15,7 @@ urlpatterns = [
     # Rutes manuals per als teus APIView d'Edificis
     path('edificis/', EdificiListAPIView.as_view(), name='edifici-list'),
     path('edificis/<str:pk>/', EdificiDetailAPIView.as_view(), name='edifici-detail'),
+    path('carrers/autocomplete/', autocomplete_carrers, name='autocomplete-carrers'),
     path('carrers/autocomplete/', autocomplete_carrers, name='autocomplete-carrers'),
     # Afegim tota la resta de rutes automàtiques del router
     path('', include(router.urls)),

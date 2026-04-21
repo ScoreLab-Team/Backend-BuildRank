e0bc5d79d882849ff3b5f67adf888fae5b55a0bd
e-d-u1 <eduard.corrons@estudiantat.upc.edu>
2026-04-21 21:36:34 +0200

    APIKEY to thirdPartyService

--- backend/apps/buildings/views.py
+++ backend/apps/buildings/views.py
@@ -26,6 +26,7 @@ from .permissions import (
     EsAdminOPropietariHabitatge,
     EsOwnerOAdminHabitatge,
     EsOwnerOAdminDadesEnergetiques,
+    HasAPIKey,
 )
 from .pagination import RankingPaginacio
  
@@ -535,11 +536,10 @@ class RankingViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class ThirdPartyServiceView(APIView):
-    permission_classes = [AllowAny]
-    
+    permission_classes = [HasAPIKey]
+
     def get(self, request):
         lat = request.query_params.get("lat")
         lng = request.query_params.get("lng")
 
-        # aquí iría tu 
         return Response(10)

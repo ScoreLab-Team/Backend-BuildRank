70543a2fe65740a3984b8e2dceb93b1b9ed31e82
martiborras <marti.borras@estudiantat.upc.edu>
2026-05-22 12:50:39 +0200

    docs: add OpenAPI schema and Swagger UI (#140)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.views.generic import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 from apps.buildings.views import ThirdPartyServiceView
@@ -37,6 +38,8 @@ urlpatterns = [
     path("api/community/", include("apps.community.urls")),
     path("api/notifications/", include("apps.notifications.urls")),
     path('api/audit/', include('apps.audit.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
 
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
